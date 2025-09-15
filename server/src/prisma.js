6ae60c81a4b2c50a905e5df6b71002128dea0eb9
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-09-15 11:43:07 +0200

    Disable extraneous-dependencies eslint

--- server/src/prisma.js
+++ server/src/prisma.js
@@ -1,14 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { PrismaClient } = require("@prisma/client");
 
 // Use a global to prevent multiple instances in dev with hot-reload
 const globalForPrisma = globalThis;
 
 let prisma;
-if (!globalForPrisma.__prisma) {
+if (!globalForPrisma.prismaClient) {
   prisma = new PrismaClient();
-  globalForPrisma.__prisma = prisma;
+  globalForPrisma.prismaClient = prisma;
 } else {
-  prisma = globalForPrisma.__prisma;
+  prisma = globalForPrisma.prismaClient;
 }
 
 module.exports = prisma;
